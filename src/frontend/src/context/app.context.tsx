927be94ab9f174a090a54b7637daa1bd251832cc
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2024-10-20 00:03:02 -0300

    added: pesisting state data on localStorage

--- src/frontend/src/context/app.context.tsx
+++ src/frontend/src/context/app.context.tsx
@@ -1,19 +1,19 @@
-import { createContext, ReactNode, useContext, useReducer } from 'react';
+import { createContext, ReactNode, useContext, useReducer, useEffect } from 'react';
 
 export interface AppState {
     userName?: string;
 }
 
 const initialState: AppState = {
-    userName: undefined,
+    userName: localStorage.getItem('userName') || undefined,
 };
-
 
 export type AppAction = { type: 'SET_USER_NAME', payload: string };
 
-function appReducer (state: AppState, action: AppAction): AppState {
+function appReducer(state: AppState, action: AppAction): AppState {
     switch (action.type) {
         case 'SET_USER_NAME':
+            localStorage.setItem('userName', action.payload);
             return { ...state, userName: action.payload };
         default:
             return state;
@@ -28,12 +28,14 @@ const AppContext = createContext<{ state: AppState; dispatch: React.Dispatch<App
 export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [state, dispatch] = useReducer(appReducer, initialState);
     const location = window.location.pathname;
-    
-    if (!["/", "/home"].includes(location)) {
-        if(state.userName == undefined){
-            window.location.href = "/home";
+
+    useEffect(() => {
+        if (!["/", "/home"].includes(location)) {
+            if (state.userName === undefined) {
+                window.location.href = "/home";
+            }
         }
-    }
+    }, [location, state.userName]);
 
     return (
         <AppContext.Provider value={{ state, dispatch }}>
